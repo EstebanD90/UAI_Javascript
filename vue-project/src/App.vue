af5d351fa3d98809d0a1bda219e0bef47a9842ca
Esteban Diaz <esteban.m.diaz90@gmail.com>
2022-06-18 02:18:57 -0300

    primer commit entradas

---
+++ vue-project/src/App.vue
@@ -0,0 +1,100 @@
+<template>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <div class="container-fluid">
+        <span class="navbar-text">Boleteria Recitales 2022</span>
+
+      </div>
+    </nav>
+  <div class="container">
+    <h1 id="titulo_boleteria">Venta de entradas</h1>
+    <div class="row">
+      <div v-for="entrada in entradasJSON" class="col-md-4">
+        <Entrada
+          :titulo="entrada.descripcion"
+          :fecha="entrada.fecha"
+          :precio="entrada.precio"
+          :lugar="entrada.lugar"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Entrada from "./components/entrada.vue";
+export default {
+  components: {
+    Entrada,
+  },
+  data() {
+    return {
+      entradasJSON: [
+        {
+          descripcion: "Green Day",
+          precio: 14500,
+          fecha: "11 de Septiembre",
+          lugar: "Vélez Sarsfield" 
+        },
+        {
+          descripcion: "Guns N'Roses",
+          precio: 17500,
+          fecha: "30 de Septiembre",
+          lugar: "River Plate" 
+        },
+        {
+          descripcion: "Las Pastillas del abuelo",
+          precio: 5000,
+          fecha: "20 de Septiembre",
+          lugar: "Luna Park" 
+        },
+        {
+          descripcion: "Coldplay",
+          precio: 69999,
+          fecha: "25 de Octubre",
+          lugar: "River Plate" 
+        },
+        {
+          descripcion: "No Te Va Gustar",
+          precio: 5500,
+          fecha: "15 de Octubre",
+          lugar: "Hipódromo de Palermo" 
+        },
+        {
+          descripcion: "Liam Gallagher",
+          precio: 9500,
+          fecha: "10 de Noviembre",
+          lugar: "Movistar Arena" 
+        }
+      ]
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+}
+
+.card-header {
+    color: whitesmoke !important;
+    font-size: 30px;
+}
+
+body {
+    background-color: #203a55 !important;
+}
+
+span {
+    color: whitesmoke !important;
+    font-size: 2em;
+}
+
+#titulo_boleteria {
+    text-align: center;
+    color: whitesmoke;
+    margin: 20px;
+}
+</style>
+
---
+++ vue-project/src/components/entrada.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="card text-bg-dark bg-dark mb-3 text-white"
+    style="max-width: 20rem"
+  >
+    <div class="card-header">{{ titulo }}</div>
+    <div class="card-body">
+      <h5 class="card-text">Fecha: {{ fecha }}</h5>
+      <p class="card-text">${{ precio }}</p>
+      <p class="card-text">Estadio: {{ lugar }}</p>
+      <p>Cantidad adquirida: {{ cantidadComprada }}</p>
+      <p>Recaudacion: {{recaudacion}}</p>
+      <button @Click="comprarEntrada()" class="btn btn-success">Comprar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {  
+    titulo: String,
+    fecha: String,
+    precio: Number,
+    lugar: String,
+  },
+  data() {
+    return {
+      cantidadComprada: 0,
+    };
+  },
+  computed: {
+    recaudacion() {
+      return this.cantidadComprada * this.precio;
+    },
+  },
+  methods: {
+    comprarEntrada() {
+      this.cantidadComprada = this.cantidadComprada + 1;
+    },
+  },
+};
+</script>
